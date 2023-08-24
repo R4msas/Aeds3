991f70b7ed76abba5f30556bfdb74a74d1cd3e2b
andre <andresalves2001@gmail.com>
2023-08-24 11:19:18 -0300

    Refatoração leitura CSV

--- TP1/src/Main.java
+++ TP1/src/Main.java
@@ -1,6 +1,5 @@
 import java.io.EOFException;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.RandomAccessFile;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,45 +11,19 @@ import java.util.Scanner;
 public class Main {
   public static String header;
 
-  public static Player[] readFromCSV(String csvFilePath) throws FileNotFoundException {
+  public static Player[] readFromCSV(String csvFilePath) throws Exception {
     Scanner scanner = new Scanner(new File(csvFilePath));
-    scanner.useDelimiter(",");
     header = scanner.nextLine();
 
     ArrayList<Player> array = new ArrayList<>();
     while (scanner.hasNext()) {
-      /* String index = */ scanner.next();
-      String nickname = scanner.next();
-      String[] teams = getTeams(scanner);
-      int playerId = scanner.nextInt();
-      String birthDate = scanner.next();
-      String country = scanner.next();
-      float rating = Float.parseFloat(scanner.nextLine().substring(1)); // Substring removes the first character(comma)
-      Player temp = new Player(nickname, teams, playerId, birthDate, country, rating);
+      Player temp = new Player();
+      temp.fromCSVLine(scanner.nextLine());
       array.add(temp);
     }
     scanner.close();
 
     return array.toArray(new Player[0]);
-  }
-
-  private static String[] getTeams(Scanner scanner) {
-    ArrayList<String> teams = new ArrayList<>();
-    teams.add(scanner.next());
-
-    if (teams.get(0).startsWith("\"")) {
-      teams.set(0, teams.get(0).substring(1)); // Removes quote from first team
-
-      String lastTeam = null;
-      do {
-        lastTeam = scanner.next().substring(1); // Substring removes the first character(space)
-        teams.add(lastTeam);
-      } while (lastTeam.endsWith("\""));
-
-      teams.set(teams.lastIndexOf(lastTeam), lastTeam.substring(0, lastTeam.length() - 1)); // Removes unquote
-    }
-
-    return teams.toArray(new String[0]);
   }
 
   public static Player[] readFromDB(String dbFilePath) throws IOException {
@@ -88,7 +61,7 @@ public class Main {
     arq.close();
   }
 
-  public static void toDBFile(String csvFilePath, String dbFilePath) throws FileNotFoundException, IOException {
+  public static void toDBFile(String csvFilePath, String dbFilePath) throws Exception {
     toDBFile(readFromCSV(csvFilePath), dbFilePath);
   }
 }

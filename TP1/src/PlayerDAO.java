4271a67970146cc75efac9c60565aeeb203f193b
andre <andresalves2001@gmail.com>
2023-08-25 10:35:11 -0300

    DAO read

--- TP1/src/PlayerDAO.java
+++ TP1/src/PlayerDAO.java
@@ -1,3 +1,4 @@
+import java.io.EOFException;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -17,17 +18,42 @@ public class PlayerDAO {
 
   public int create(Player player) throws IOException {
     raf.seek(0);
-
     player.setPlayerId(raf.readInt() + 1);
+
     raf.seek(0);
-    raf.writeInt(player.getPlayerId());
+    raf.writeInt(player.getPlayerId()); // Set header
 
     raf.seek(raf.length());
+
+    /// TODO Refactoring
     byte[] ba = player.toByteArray();
+    raf.writeBoolean(true); // Writes tombstone
     raf.writeInt(ba.length);
     raf.write(ba);
 
     return player.getPlayerId();
+  }
+
+  public Player read(int id) throws IOException {
+    raf.seek(Integer.SIZE);
+    long position = raf.getFilePointer();
+
+    while (position < raf.length()) {
+      boolean tombstone = raf.readBoolean();
+      int registerSize = raf.readInt();
+
+      if (tombstone == false) {
+        position += registerSize;
+        raf.seek(position);
+        continue;
+      }
+
+      // TODO Refactoring
+      byte[] bytes = new byte[registerSize];
+      raf.read(bytes);
+      Player temp = new Player();
+      temp.fromByteArray(bytes);
+    }
   }
 
   public void close() throws IOException {

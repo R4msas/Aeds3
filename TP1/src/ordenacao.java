0c0430379a2a961f104015d257baf6b29e010db8
Allan Gomes <113448955+R4msas@users.noreply.github.com>
2023-08-26 19:28:43 -0300

    começo da classe ordenação, método leituraInicial ainda não testado

--- TP1/src/ordenacao.java
+++ TP1/src/ordenacao.java
@@ -51,34 +51,43 @@ public ordenacao(long ponteiroArquivo, ArrayList<Long> ponteiroTmp, int numTmp) 
         this.numTmp = numTmp;
     }
 //lê o arquivo inicial e a cada 20 registros alterna entre a escrita nos arquivos tmp0 e tmp1
-public ArrayList<Long> leituraInicial(String nomeArquivoPrincipal)throws IOException{
-    String nomeArquivo;
-    String tmp="tmp";
-    int repeticao=0;
-    RandomAccessFile tmp0=new RandomAccessFile("tmp0","w");
-    RandomAccessFile tmp1=new RandomAccessFile("tmp1","w");
+public void leituraInicial(String nomeArquivoPrincipal)throws IOException{
+    int numArquivo=0, repeticao=0;
+    RandomAccessFile tmp[]=new RandomAccessFile[2];
+    tmp[0]=new RandomAccessFile("tmp0","w");
+    tmp[1]=new RandomAccessFile("tmp1","w");
     RandomAccessFile arqPrincipal=new RandomAccessFile(nomeArquivoPrincipal,"r");
-    int ultimoId=arqPrincipal.readInt();
-    int id;
-    ArrayList<Long> ponteiros=new ArrayList<Long>;
+    arqPrincipal.readInt();
+    long ponteiroPrincipal=arqPrincipal.getFilePointer();
+    do
+        {
+            repeticao=0;
+           while(repeticao<20&&ponteiroPrincipal<arqPrincipal.length())
+            {
+                ponteiroPrincipal=arqPrincipal.getFilePointer();
+                Boolean lapide=arqPrincipal.readBoolean();
+                int tam=arqPrincipal.readInt();
+                if(lapide==false)
+                {
+                    byte[] b=new byte[tam];
+                    arqPrincipal.read(b);
+                    tmp[numArquivo].write(b);
+                    repeticao++;
+                }
+                else{
+                    arqPrincipal.skipBytes(tam);
+                }
+            }
+            numArquivo=(numArquivo-1)*-1;//alterna entre 0 e 1 para salvar no arquivo tmp 0 e 1;
+        }while(ponteiroPrincipal<arqPrincipal.length());
 
-
-do
-{
-    id=arqPrincipal.readInt();
-    nomeArquivo=tmp+repeticao;
-    for (int c=0;c<20;c++)
-    {
-        
-        
-
-    }
-    repeticao=(repeticao-1)*-1;//alterna entre 0 e 1 para salvar no arquivo tmp 0 e 1;
-}while();
-
-tmp0.close();
-tmp1.close();
+tmp[0].close();
+tmp[1].close();
 arqPrincipal.close();
 }
     
+public void leVinteRegistrosEOrdena (String nomeArq)
+{
+    
+}
 }
